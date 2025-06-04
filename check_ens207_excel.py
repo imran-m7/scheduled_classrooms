42db8fd483939aada28314f90448742e2bb7f365
imran-m7 <imran.mujkanovic@gmail.com>
2025-06-05 00:14:58 +0200

    workspace uljepsan

--- check_ens207_excel.py
+++
@@ -1,6 +0,0 @@
-import openpyxl
-
-wb = openpyxl.load_workbook('course_assignments.xlsx')
-ws = wb.active
-for row in ws.iter_rows(min_row=2, values_only=True):
-    print(f'{row[0]}: Enrollment={row[3]}')
