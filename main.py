846351a16aa8143d9df9dd1fb71e0080161831f1
imran-m7 <imran.mujkanovic@gmail.com>
2025-06-04 13:14:32 +0200

    assignani arch109 zajedno

--- main.py
+++ main.py
@@ -687,6 +687,28 @@ def main():
                         prob += x[c2, b_f1_10_room, t] == 0
     # --- End preferred assignment for B F1.10 Class/ART Studio courses ---
 
+    # --- Special case: Force ARCH109.1 and ARCH109.2 to overlap in A F3.10 - Architecture Classroom ---
+    arch109_courses = ['ARCH109.1', 'ARCH109.2']
+    arch109_room = 'A F3.10 - Architecture Classroom'
+    # Find all times where both courses are scheduled
+    arch109_times = set(course_times.get('ARCH109.1', [])) & set(course_times.get('ARCH109.2', []))
+    for t in arch109_times:
+        # Force both courses to be assigned to A F3.10 at this time
+        for course in arch109_courses:
+            for r in rooms:
+                if r == arch109_room:
+                    if (course, r, t) in x:
+                        prob += x[course, r, t] == 1
+                else:
+                    if (course, r, t) in x:
+                        prob += x[course, r, t] == 0
+        # Block all other courses from this room at this time
+        for c2 in courses:
+            if c2 not in arch109_courses and t in course_times.get(c2, []):
+                if (c2, arch109_room, t) in x:
+                    prob += x[c2, arch109_room, t] == 0
+    # --- End special case for ARCH109.1 and ARCH109.2 ---
+
     # --- Add preferred assignment for A F3.10 - Architecture Classroom courses ---
     a_f3_10_room = 'A F3.10 - Architecture Classroom'
     a_f3_10_courses_set = set(['ARCH510.1', 'ARCH517.1', 'ARCH569.1', 'ARCH101.1', 'ARCH307.1', 'ARCH304.1'])
@@ -958,6 +980,14 @@ def main():
                 assigned_to_big_arch = (assigned_room1 == big_arch_room) or (assigned_room2 == big_arch_room)
                 if assigned_to_big_arch:
                     status = 'Assigned (Big Architecture Studio)'
+                else:
+                    infeasible = all(enrollment > capacities[r] for r in rooms)
+                    status = 'Infeasible' if infeasible else 'Unassigned'
+            # --- Assignment status for ARCH109.1 and ARCH109.2 ---
+            elif c in ['ARCH109.1', 'ARCH109.2']:
+                assigned_to_f3_10 = (assigned_room1 == a_f3_10_room) or (assigned_room2 == a_f3_10_room)
+                if assigned_to_f3_10:
+                    status = 'Assigned (A F3.10 - Architecture Classroom)'
                 else:
                     infeasible = all(enrollment > capacities[r] for r in rooms)
                     status = 'Infeasible' if infeasible else 'Unassigned'
